a89786aef24ebdf28d201a664f3eff4657f61c37
mckim <kmc04@naver.com>
2021-09-08 02:38:41 +0900

    Topbar 추가 - 항상 고정(sticky)

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import "./topbar.css"
+import {NotificationsNone, Language, Settings} from '@material-ui/icons';
+
+export default function Topbar() {
+    return (
+        <div className="topbar">
+            <div className="topbarWrapper">
+                <div className="topLeft">
+                    <span className="logo">어드민</span>
+                </div>
+                <div className="topRight">
+                    <div className="topbarIconContainer">
+                        <NotificationsNone />
+                        <span className="topIconBadge">2</span>
+                    </div>
+                    <div className="topbarIconContainer">
+                        <Language />
+                        <span className="topIconBadge">2</span>
+                    </div>
+                    <div className="topbarIconContainer">
+                        <Settings />
+                    </div>
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bc/Unknown_person.jpg/542px-Unknown_person.jpg" alt="" className="topAvatar" />
+                </div>
+            </div>
+        </div>
+    )
+}
