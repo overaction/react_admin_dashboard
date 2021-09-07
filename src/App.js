e6ab579b598244ea338457aa01fdcde3559e2dea
mckim <kmc04@naver.com>
2021-09-08 03:37:22 +0900

    사이드바 제작

--- src/App.js
+++ src/App.js
@@ -1,104 +1,14 @@
+import Sidebar from './components/sidebar/Sidebar';
 import Topbar from './components/topbar/Topbar';
-
+import "./app.css"
 function App() {
   return (
     <div className="App">
       <Topbar />
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      
+      <div className="container">
+        <Sidebar />
+        <div className="others">다른 페이지들</div>
+      </div>
     </div>
   );
 }
---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,83 @@
+import "./sidebar.css"
+import {LineStyle,Timeline, TrendingUp, PermIdentity, Storefront, AttachMoney, BarChart, MailOutline, DynamicFeed, ChatBubbleOutline,WorkOutline,Report} from '@material-ui/icons';
+
+export default function Sidebar() {
+    return (
+        <div className="sidebar">
+            <div className="sidebarWrapper">
+                <div className="sidebarMenu">
+                    <h3 className="sidebarTitle">대쉬보드</h3>
+                    <ul className="sidebarList">
+                        <li className="sidebarListItem active">
+                            <LineStyle className="sidebarIcon"/>
+                            홈
+                        </li>
+                        <li className="sidebarListItem">
+                            <Timeline className="sidebarIcon"/>
+                            분석
+                        </li>
+                        <li className="sidebarListItem">
+                            <TrendingUp className="sidebarIcon"/>
+                            추이
+                        </li>
+                    </ul>
+                </div>
+                <div className="sidebarMenu">
+                    <h3 className="sidebarTitle">퀵 메뉴</h3>
+                    <ul className="sidebarList">
+                        <li className="sidebarListItem active">
+                            <PermIdentity className="sidebarIcon"/>
+                            유저
+                        </li>
+                        <li className="sidebarListItem">
+                            <Storefront className="sidebarIcon"/>
+                            제품
+                        </li>
+                        <li className="sidebarListItem">
+                            <AttachMoney className="sidebarIcon"/>
+                            거래
+                        </li>
+                        <li className="sidebarListItem">
+                            <BarChart className="sidebarIcon"/>
+                            통계
+                        </li>
+                    </ul>
+                </div>
+                <div className="sidebarMenu">
+                    <h3 className="sidebarTitle">알림</h3>
+                    <ul className="sidebarList">
+                        <li className="sidebarListItem active">
+                            <MailOutline className="sidebarIcon"/>
+                            메일
+                        </li>
+                        <li className="sidebarListItem">
+                            <DynamicFeed className="sidebarIcon"/>
+                            피드백
+                        </li>
+                        <li className="sidebarListItem">
+                            <ChatBubbleOutline className="sidebarIcon"/>
+                            메시지
+                        </li>
+                    </ul>
+                </div>
+                <div className="sidebarMenu">
+                    <h3 className="sidebarTitle">스태프</h3>
+                    <ul className="sidebarList">
+                        <li className="sidebarListItem active">
+                            <WorkOutline className="sidebarIcon"/>
+                            관리
+                        </li>
+                        <li className="sidebarListItem">
+                            <Timeline className="sidebarIcon"/>
+                            분석
+                        </li>
+                        <li className="sidebarListItem">
+                            <Report className="sidebarIcon"/>
+                            리포트
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+}
