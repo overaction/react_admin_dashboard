d06b03d25efdfa3e7092867ae810edfb89fed0d6
mckim <kmc04@naver.com>
2021-09-09 01:47:29 +0900

    최근 거래를 나타내는 widgetLarge

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,12 +3,18 @@ import Chart from '../../components/chart/Chart'
 import FeaturedInfo from '../../components/featuredInfo/FeaturedInfo'
 import "./home.css";
 import {userData} from "../../dummyData";
+import WidgetSmall from '../../components/widget-small/WidgetSmall';
+import WidgetLarge from '../../components/widget-large/WidgetLarge';
 
 export default function Home() {
     return (
         <div className="home">
             <FeaturedInfo />
             <Chart title={`사용자 분석`} data={userData} dataKey={`Active User`} grid/>
+            <div className="homeWidgets">
+                <WidgetSmall />
+                <WidgetLarge />
+            </div>
         </div>
     )
 }
