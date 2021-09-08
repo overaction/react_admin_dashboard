ab5cd1b1a5fd679024fa3b6b71abfb2fae4719d0
mckim <kmc04@naver.com>
2021-09-09 03:39:09 +0900

    유저 관리 페이지 생성 (+ react-router-dom)

--- src/App.js
+++ src/App.js
@@ -2,15 +2,25 @@ import Sidebar from './components/sidebar/Sidebar';
 import Topbar from './components/topbar/Topbar';
 import Home from './pages/home/Home'
 import "./app.css"
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import UserList from './pages/userList/UserList';
+
 function App() {
   return (
-    <div className="App">
-      <Topbar />
-      <div className="container">
-        <Sidebar />
-        <Home />
-      </div>
-    </div>
+      <Router>
+          <Topbar />
+          <div className="container">
+              <Sidebar />
+              <Switch>
+                <Route exact path="/">
+                  <Home />
+                </Route>
+                <Route path="/users">
+                  <UserList />
+                </Route>
+              </Switch>
+          </div>
+      </Router>
   )
 }
 
