9b159d68cbc3af60077ab423125c985fd6a9e2d7
mckim <kmc04@naver.com>
2021-09-09 23:53:31 +0900

    상품 리스트 페이지

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import UserList from './pages/userList/UserList';
 import User from './pages/user/User';
 import NewUser from './pages/newUser/NewUser';
+import ProductList from './pages/productList/ProductList';
 
 function App() {
   return (
@@ -25,6 +26,15 @@ function App() {
                 </Route>
                 <Route path="/newUser">
                   <NewUser />
+                </Route>
+                <Route path="/products">
+                  <ProductList />
+                </Route>
+                <Route exact path="/products/:productsId">
+                  <ProductList />
+                </Route>
+                <Route path="/newproduct">
+                  <ProductList />
                 </Route>
               </Switch>
           </div>
